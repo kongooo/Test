0ba1ddae0ceb85385f3dd49d9d38bb6c29df955c
kongooo <1184701824@qq.com>
2020-03-24 17:12:11 +0800

    test

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
-        publicPath: '/'
+        publicPath: './'
     },
     devtool: 'source-map',
     mode: 'development',
